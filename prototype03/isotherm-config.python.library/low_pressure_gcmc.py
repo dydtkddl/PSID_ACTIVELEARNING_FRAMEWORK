f526f87d17402d7f87264a78c3865d0e3b55dff0
dydtkddl <dydtkddhkdwk@naver.com>
2025-06-13 16:54:51 +0900

    123

--- prototype03/isotherm-config.python.library/low_pressure_gcmc.py
+++ prototype03/isotherm-config.python.library/low_pressure_gcmc.py
@@ -42,6 +42,7 @@ LOG_FILE = LOG_DIR / f'run_{hostname}.log'
 uptake_log_file = LOG_DIR / f'uptake_{hostname}.log'
 error_log_file = LOG_DIR / f'error_{hostname}.log'
 # Uptake logger (uptake.log)
+run_logger = logging.getLogger('gcmc_run')
 uptake_logger = logging.getLogger('gcmc_uptake')
 uptake_handler = logging.FileHandler(uptake_log_file)
 uptake_handler.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
